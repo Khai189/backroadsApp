af6594f60a36705d15e7c56cdb6d5ef4adcdf901
Khai189 <khmr2024@mymail.pomona.edu>
2026-02-25 22:23:01 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Tours from "./components/Tours";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Service from "./components/Service";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Tours/>
+      <About/>
+      <Service/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
